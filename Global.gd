7c31697d50ed8db07413c9c1cbaa75509a0379c6
maxmorrow12 <maxamorr@iu.edu>
2023-11-27 16:54:29 -0500

    Completes Exercise

--- Global.gd
+++ Global.gd
@@ -1,5 +1,10 @@
 extends Node
 
-func _unhandled_input(_event):
-	if Input.is_action_just_pressed("menu"):
-		get_tree().quit()
+func shoot(): 
+	var Player = get_node_or_null("/root/Game/Player")
+	if Player != null:
+		Player.shoot()
+var Zombie = get_node_or_null("/root/Game/Zombie")
+	if Zombie != null:
+		Zombie.die()
+
